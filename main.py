a77be82d0f5df107677f33297958d4f768615407
a-romash <romashov.artemiy@gmail.com>
2022-12-26 16:24:12 +0300

    Created black window

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
+import pygame
+
+SIZE = WIDTH, HEIGHT = 1280, 720
+FPS = 30
+
 if __name__ == "__main__":
-    print('123')
+    pygame.init()
+
+    screen = pygame.display.set_mode(SIZE)
+    clock = pygame.time.Clock()
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill((0, 0, 0))
+        clock.tick(FPS)
+        pygame.display.flip()
+    pygame.quit()
