257a731855a8c06d3bb4d08d6e6f86280fcfc121
Artem Romashov <artem20071014@gmail.com>
2023-01-10 21:52:56 +0300

    refactored variables added new const in main.py

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ from startMenu import start_menu
 
 SIZE = WIDTH, HEIGHT = 1280, 720
 FPS = 30
+# 0 - Начальное меню
+# 1 - Правила
+# 2 - Что то после "Играть"
+UI_CONDITION = 0
+
 
 if __name__ == "__main__":
     pygame.init()
@@ -17,8 +22,9 @@ if __name__ == "__main__":
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-
-        start_menu(screen)
+        
+        if UI_CONDITION == 0:
+            start_menu(screen)
 
         screen.fill((0, 0, 0))
         clock.tick(FPS)
