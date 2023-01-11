ab0ca5b990f97f9a53f4a1c6a41b2c8d8fd52189
Artem Romashov <artem20071014@gmail.com>
2023-01-11 20:48:23 +0300

    added idle animation

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
+import os
 import pygame
-import spriteAnimation
+from spriteAnimation import *
 
 from startMenu import start_menu
 
 SIZE = WIDTH, HEIGHT = 1280, 720
-FPS = 30
+FPS = 5
 # 0 - Начальное меню
 # 1 - уже сама игра
 UI_CONDITION = 0
 
 
+def load_image(name, color_key=None):
+    fullname = os.path.join(name)
+    try:
+        image = pygame.image.load(fullname)
+    except pygame.error as message:
+        print('Не удаётся загрузить:', name)
+        raise SystemExit(message)
+    if color_key is not None:
+        if color_key == -1:
+            color_key = image.get_at((0, 0))
+        image.set_colorkey(color_key)
+    return image
+
+
 if __name__ == "__main__":
     pygame.init()
+
+    all_sprites = pygame.sprite.Group()
+    character = AnimatedSprite(all_sprites,
+                               load_image(os.path.join("assets", "characters", "ninja", "character", "Idle.png")), 4,
+                               100, SIZE[1] - 500)
+    all_sprites.add(character)
 
     screen = pygame.display.set_mode(SIZE)
     clock = pygame.time.Clock()
@@ -21,11 +42,9 @@ if __name__ == "__main__":
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-
-        if UI_CONDITION == 0:
-            start_menu(screen)
-
-        screen.fill((0, 0, 0))
+        screen.fill(pygame.Color("white"))
+        all_sprites.draw(screen)
+        all_sprites.update()
         clock.tick(FPS)
         pygame.display.flip()
     pygame.quit()
--- spriteAnimation.py
+++ spriteAnimation.py
@@ -5,6 +5,7 @@ class AnimatedSprite(pygame.sprite.Sprite):
     def __init__(self, sprite_group, sheet, columns, x, y):
         super().__init__(sprite_group)
         self.frames = []
+        sheet = pygame.transform.scale(sheet, (sheet.get_width() * 2, sheet.get_height() * 2))
         self.cut_sheet(sheet, columns)
         self.cur_frame = 0
         self.image = self.frames[self.cur_frame]
@@ -14,7 +15,7 @@ class AnimatedSprite(pygame.sprite.Sprite):
         self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
                                 sheet.get_height())
         for i in range(columns):
-            frame_location = (self.rect.w * i, self.rect.h * j)
+            frame_location = (self.rect.w * i, 0)
             self.frames.append(sheet.subsurface(pygame.Rect(
                 frame_location, self.rect.size)))
 
